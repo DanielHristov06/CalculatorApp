f52fdb32639b1c4ae286d4b330c660d18d626114
Daniel Hristov <dhristov06@gmail.com>
2025-08-22 01:46:30 +0300

    Added a secondary display for the expression

--- include/wxwidgets/MainFrame.h
+++ include/wxwidgets/MainFrame.h
@@ -6,10 +6,13 @@ public:
 	MainFrame(const wxString& title, const wxSize& size);
 
 private:
+	wxTextCtrl* mExpr = nullptr;
 	wxTextCtrl* mDisplay = nullptr;
 
 	void OnButton(wxCommandEvent& e);
 	void OnClear(wxCommandEvent& e);
 	void OnBackspace(wxCommandEvent& e);
 	void OnEquals(wxCommandEvent& e);
+
+	bool IsOp(const wxChar& c);
 };
--- src/MainFrame.cpp
+++ src/MainFrame.cpp
@@ -8,9 +8,13 @@ MainFrame::MainFrame(const wxString& title, const wxSize& size) : wxFrame(nullpt
 	wxPanel* panel = new wxPanel(this);
 	wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
 
+	mExpr = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_READONLY | wxTE_RIGHT | wxBORDER_NONE);
+	mExpr->SetFont(wxFontInfo(18));
+	vbox->Add(mExpr, 0, wxEXPAND | wxRIGHT | wxTOP | wxBOTTOM, 25);
+
 	mDisplay = new wxTextCtrl(panel, wxID_ANY, "0", wxDefaultPosition, wxDefaultSize, wxTE_READONLY | wxTE_RIGHT | wxBORDER_NONE);
 	mDisplay->SetFont(wxFontInfo(24));
-	vbox->Add(mDisplay, 0, wxEXPAND | wxLEFT | wxTOP | wxBOTTOM, 25);
+	vbox->Add(mDisplay, 0, wxEXPAND | wxRIGHT | wxTOP | wxBOTTOM, 25);
 
 	wxGridSizer* grid = new wxGridSizer(0, 4, 5, 5);
 
@@ -39,15 +43,54 @@ MainFrame::MainFrame(const wxString& title, const wxSize& size) : wxFrame(nullpt
 }
 
 void MainFrame::OnButton(wxCommandEvent& e) {
-	wxButton* b = wxDynamicCast(e.GetEventObject(), wxButton);
+	const wxButton* b = wxStaticCast(e.GetEventObject(), wxButton);
 	if (!b) return;
-	const wxString t = b->GetLabel();
-	if (mDisplay->GetValue() == "0") mDisplay->Clear();
-	mDisplay->AppendText(t);
+	const wxString label = b->GetLabel();
+
+	if (label.size() == 1 && IsOp(label[0])) {
+		const wxChar op = label[0];
+
+		const wxString cur = mDisplay->GetValue();
+		if (!(cur == "0" || cur.empty())) {
+			mExpr->AppendText(cur);
+			mDisplay->SetValue("0");
+		}
+
+		wxString expr = mExpr->GetValue();
+		if (expr.empty()) return;
+
+		if (!expr.empty() && IsOp(expr.Last())) {
+			expr.Last() = op;
+			mExpr->SetValue(expr);
+		}
+		else {
+			mExpr->AppendText(wxString(op));
+		}
+		return;
+	}
+
+	if (label == "(") {
+		mExpr->AppendText(label);
+		return;
+	}
+	else if (label == ")") {
+		if (!IsOp(mExpr->GetValue().Last()) && mExpr->GetValue().Last() != '(') {
+			mExpr->AppendText(label);
+			return;
+		}
+		else return;
+	}
+
+	if (mDisplay->GetValue() == "0" && label != ".") {
+		mDisplay->Clear();
+	}
+
+	mDisplay->AppendText(label);
 }
 
 void MainFrame::OnClear(wxCommandEvent& e) {
 	mDisplay->SetValue("0");
+	mExpr->SetValue("");
 }
 
 void MainFrame::OnBackspace(wxCommandEvent& e) {
@@ -57,6 +100,12 @@ void MainFrame::OnBackspace(wxCommandEvent& e) {
 }
 
 void MainFrame::OnEquals(wxCommandEvent& e) {
-	const std::string expr = mDisplay->GetValue().ToStdString();
+	mExpr->AppendText(mDisplay->GetValue());
+	const std::string expr = mExpr->GetValue().ToStdString();
 	mDisplay->SetValue(wxString::Format("%g", calc::evaluate(expr)));
+	mExpr->SetValue("");
+}
+
+bool MainFrame::IsOp(const wxChar& c) {
+	return wxString("+-*/").Find(c) != wxNOT_FOUND;
 }
